23904c6aa456792e02f4239548af0f0472aa76d2
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-07 22:39:39 +0530

    added line length calculation(UC1)

--- src/javafundamentals/LineComparison.java
+++ src/javafundamentals/LineComparison.java
@@ -1,7 +1,39 @@
 package javafundamentals;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+        Scanner scanner = new Scanner(System.in);
+
+        //input for line 1
+        System.out.println("Enter Line 1 coordinates (x1 y1 x2 y2): ");
+        double x1_1 = scanner.nextDouble();
+        double y1_1 = scanner.nextDouble();
+        double x2_1 = scanner.nextDouble();
+        double y2_1 = scanner.nextDouble();
+
+        //input for line 2
+        System.out.println("Enter Line 2 coordinates (x1 y1 x2 y2): ");
+        double x1_2 = scanner.nextDouble();
+        double y1_2 = scanner.nextDouble();
+        double x2_2 = scanner.nextDouble();
+        double y2_2 = scanner.nextDouble();
+
+        // Calculating lengths
+        double line1Length = calculateLength(x1_1, y1_1, x2_1, y2_1);
+        double line2Length = calculateLength(x1_2, y1_2, x2_2, y2_2);
+
+        System.out.println("\nLength of Line 1 = " + line1Length);
+        System.out.println("Length of Line 2 = " + line2Length);
+    }
+
+    // UC1: Length = sqrt((x2-x1)^2 + (y2-y1)^2)
+    private static double calculateLength(double x1, double y1, double x2, double y2) {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
