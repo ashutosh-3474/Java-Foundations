d994e5af5eae8ec3def3b0e8747f870662f72f6f
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-09 15:58:47 +0530

    Added employee attendance check using random (UC1)

---
+++ src/javafundamentals/employeewagecomputation/Employee.java
@@ -0,0 +1,18 @@
+package javafundamentals.employeewagecomputation;
+
+public class Employee {
+    int dailyAttendance;
+
+    public void calculateAttendance(){
+        if (Math.random() < 0.5)
+        {
+            dailyAttendance = 0;
+            System.out.println("Absent");
+        }
+        else
+        {
+            dailyAttendance = 1;
+            System.out.println("Present");
+        }
+    }
+}
--- src/javafundamentals/employeewagecomputation/Main.java
+++ src/javafundamentals/employeewagecomputation/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the employee wage computation program.");
+        Employee e1 = new Employee();
+        e1.calculateAttendance();
     }
 }
