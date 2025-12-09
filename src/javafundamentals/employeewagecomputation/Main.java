03714687ba9f6f6091c0c2fdb148672e9987f276
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-09 15:41:37 +0530

    reinitialising the employee wage computation program

---
+++ src/javafundamentals/employeewagecomputation/Main.java
@@ -0,0 +1,8 @@
+package javafundamentals.employeewagecomputation;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the employee wage computation program.");
+    }
+}
